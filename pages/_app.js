d6b72fed7077e43ff2335ed1c1ea061db9fe8821
Simonas Kareiva <simonas@5grupe.lt>
2021-12-11 21:28:00 +0200

    Refresh app every 120s

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '@styles/globals.css'
+import { SWRConfig } from 'swr'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig value={{
+        dedupingInterval: 120000,
+        refreshInterval: 120000,
+        revalidateOnFocus: false
+      }}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  )
 }
 
 export default Application
